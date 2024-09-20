0ecdf8c74a8de30ac37396ea165233190cb41f24
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-09-21 05:32:44 +0545

    Schedule of counting contributions every day at midnight transfer it to index.js

--- count-contributors.ts
+++ count-contributors.ts
@@ -98,14 +98,3 @@ async function run() {
 }
 
 export default run;
-
-cron.schedule("* * * * *", async () => {
-	console.log("Starting...");
-	const { totalCount, commitCount, issueCount } = await run();
-
-	await Bun.write("data/total-count.json", JSON.stringify(totalCount));
-	await Bun.write("data/commit-count.json", JSON.stringify(commitCount));
-	await Bun.write("data/issue-count.json", JSON.stringify(issueCount));
-
-	console.log("Ended");
-});
