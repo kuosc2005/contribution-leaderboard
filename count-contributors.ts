f01e9009ecab71bfee7b476baf1c37071f819396
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-09-21 06:02:08 +0545

    update the data like in previous api

--- count-contributors.ts
+++ count-contributors.ts
@@ -1,100 +1,124 @@
-import cron from "node-cron";
-
 // Fetch headers
 const headers = {
-	Authorization: `token ${process.env.TOKEN}`,
+  Authorization: `token ${process.env.TOKEN}`,
 };
 
 /// Get total, commit and issue count inside an organization or a user (username)
 async function getCountBasedOnOrg(
-	orgName: string,
-	commitCount: { [key: string]: number },
-	issueCount: { [key: string]: number },
+  orgName: string,
+  commitCount: { [key: string]: number },
+  issueCount: { [key: string]: number },
+  userDetails: { [key: string]: { userId: number; username: string } },
 ) {
-	// Calculate for the first of every year
-	const currentDate = new Date().getFullYear();
-	const countStartDate = new Date(`${currentDate}-01-01`);
+  // Calculate for the first of every year
+  const currentDate = new Date().getFullYear();
+  const countStartDate = new Date(`${currentDate}-01-01`);
 
-	// Fetch the repositories of the organization or the user
-	const res = await fetch(
-		`https://api.github.com/orgs/${orgName}/repos?per_page=100`,
-		{ headers },
-	);
-	const data = await res.json();
-	const repos: string[] = data.map((item: any) => item.name);
+  // Fetch the repositories of the organization or the user
+  const res = await fetch(
+    `https://api.github.com/orgs/${orgName}/repos?per_page=100`,
+    { headers },
+  );
+  const data = await res.json();
+  const repos: string[] = data.map((item: any) => item.name);
 
-	for (const repo of repos) {
-		if (!repo.includes("scrapemore") && repo !== "it-meet") {
-			// Fetch commits for the repo
-			const res = await fetch(
-				`https://api.github.com/repos/${orgName}/${repo}/commits?since=${countStartDate.toISOString()}&per_page=500`,
-				{ headers },
-			);
-			const data = await res.json();
+  for (const repo of repos) {
+    if (!repo.includes("scrapemore") && repo !== "it-meet") {
+      // Fetch commits for the repo
+      const res = await fetch(
+        `https://api.github.com/repos/${orgName}/${repo}/commits?since=${countStartDate.toISOString()}&per_page=500`,
+        { headers },
+      );
+      const data = await res.json();
 
-			// Update the commit count
-			if (res.status === 200) {
-				data.forEach((commit: any) => {
-					const authorName = commit.author.login;
-					if (commitCount[authorName]) {
-						commitCount[authorName]++;
-					} else {
-						commitCount[authorName] = 1;
-					}
-				});
-			}
+      // Update the commit count
+      if (res.status === 200) {
+        for (const commit of data) {
+          const author = commit.author;
+          if (author) {
+            const authorName = author.login;
+            const userId = author.id;
+            if (!userDetails[authorName]) {
+              userDetails[authorName] = { userId, username: authorName };
+            }
 
-			// Fetch issues for the repo
-			const issueRes = await fetch(
-				`https://api.github.com/repos/${orgName}/${repo}/issues?since=${countStartDate.toISOString()}`,
-				{ headers },
-			);
-			const issueData = await issueRes.json();
+            commitCount[authorName] = (commitCount[authorName] || 0) + 1;
+          }
+        }
+      }
 
-			// Update the issue count
-			if (issueRes.status === 200) {
-				issueData.forEach((issue: any) => {
-					const userLogin = issue.user.login;
-					if (issueCount[userLogin]) {
-						issueCount[userLogin]++;
-					} else {
-						issueCount[userLogin] = 1;
-					}
-				});
-			}
-		}
-	}
+      // Fetch issues for the repo
+      const issueRes = await fetch(
+        `https://api.github.com/repos/${orgName}/${repo}/issues?since=${countStartDate.toISOString()}`,
+        { headers },
+      );
+      const issueData = await issueRes.json();
+
+      // Update the issue count
+      if (issueRes.status === 200) {
+        for (const issue of issueData) {
+          const user = issue.user;
+          const userName = user.login;
+          const userId = user.id;
+          if (!userDetails[userName]) {
+            userDetails[userName] = { userId, username: userName };
+          }
+
+          issueCount[userName] = (issueCount[userName] || 0) + 1;
+        }
+      }
+    }
+  }
 }
 
 async function run() {
-	const orgs = ["kucc1997", "kuosc2005"];
+  const orgs = ["kucc1997", "kuosc2005"];
 
-	let commitCount: Record<string, number> = {};
-	let issueCount: Record<string, number> = {};
+  let commitCount: Record<string, number> = {};
+  let issueCount: Record<string, number> = {};
+  let userDetails: Record<string, { userId: number; username: string }> = {};
 
-	for (const org of orgs) {
-		await getCountBasedOnOrg(org, commitCount, issueCount);
-	}
+  for (const org of orgs) {
+    await getCountBasedOnOrg(org, commitCount, issueCount, userDetails);
+  }
 
-	let totalCount: Record<string, number> = { ...commitCount };
-	for (const [key, value] of Object.entries(issueCount)) {
-		if (commitCount[key]) {
-			totalCount[key] += value;
-		} else {
-			totalCount[key] = value;
-		}
-	}
-	totalCount = Object.entries(totalCount)
-		.sort(([, countA], [, countB]) => countB - countA)
-		.reduce(
-			(acc, [user, count]) => {
-				acc[user] = count;
-				return acc;
-			},
-			{} as Record<string, number>,
-		);
+  let totalCount: Record<string, number> = { ...commitCount };
+  for (const [key, value] of Object.entries(issueCount)) {
+    if (commitCount[key]) {
+      totalCount[key] += value;
+    } else {
+      totalCount[key] = value;
+    }
+  }
 
-	return { totalCount, commitCount, issueCount };
+  const result = Object.entries(totalCount)
+    .map(([username, total]) => {
+      const types = [];
+      if (commitCount[username]) {
+        types.push({
+          type: "commit",
+          count: commitCount[username],
+        });
+      }
+      if (issueCount[username]) {
+        types.push({
+          type: "issue",
+          count: issueCount[username],
+        });
+      }
+
+      return {
+        _id: {
+          userId: userDetails[username].userId,
+          username: userDetails[username].username,
+        },
+        types,
+        total,
+      };
+    })
+    .sort((a, b) => b.total - a.total);
+
+  return result;
 }
 
 export default run;
--- index.ts
+++ index.ts
@@ -1,25 +1,22 @@
 import cron from "node-cron";
 import run from "./count-contributors";
 
-// Schedule of u
+// Schedule of counting contributions every day at midnight
 cron.schedule("0 0 * * *", async () => {
-	console.log("Started counting");
-	const { totalCount, commitCount, issueCount } = await run();
+  console.log("Started counting");
+  const data = await run();
 
-	console.log("Writing to files...");
+  console.log("Writing to files...");
+  await Bun.write("data.json", JSON.stringify(data));
 
-	await Bun.write("data/total-count.json", JSON.stringify(totalCount));
-	await Bun.write("data/commit-count.json", JSON.stringify(commitCount));
-	await Bun.write("data/issue-count.json", JSON.stringify(issueCount));
-
-	console.log("Ended counting");
+  console.log("Ended counting");
 });
 
 const server = Bun.serve({
-	port: process.env.PORT || 3000,
-	fetch(_req) {
-		return new Response("Bun!");
-	},
+  port: process.env.PORT || 3000,
+  fetch(_req) {
+    return new Response("Bun!");
+  },
 });
 
 console.log(`Listening on http://localhost:${server.port} ...`);
