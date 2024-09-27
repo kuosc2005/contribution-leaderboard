714bf3cc3c71eb38dda43d9b92156e51239ea625
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-09-27 17:28:18 +0545

    List all the repos to count instead of fetching all the repos

---
+++ repos.ts
@@ -0,0 +1,17 @@
+const kuoscRepos = [
+  "kuosc2005/contribution-leaderboard",
+  "kuosc2005/website",
+  "kuosc2005/sfd-website",
+  "kuosc2005/KUevents",
+  "kuosc2005/certifly",
+  "kuosc2005/merchandise-design",
+];
+
+const kuccRepos = [
+  "kucc1997/kucc-website",
+  "kucc1997/KUCC_BOT_FINAL",
+  "kucc1997/KUCCApp",
+  "kucc1997/ku-projects" < "kucc1997/HeNN-E-Library-Documentation",
+];
+
+export default [...kuoscRepos, kuccRepos];
